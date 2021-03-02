fa74d2c78aa483aed1f4b03d030d68be4f7c460c
sirGrishin <79722832+sirGrishin@users.noreply.github.com>
2021-03-02 16:24:26 +0200

    Update Main.java

--- CoreTaskTemplate/src/main/java/jm/task/core/jdbc/Main.java
+++ CoreTaskTemplate/src/main/java/jm/task/core/jdbc/Main.java
@@ -8,17 +8,13 @@ import jm.task.core.jdbc.util.Util;
 public class Main {
     public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
-//        userService.createUsersTable();
-//        userService.saveUser("Artour", "Babai", (byte) 23);
-//        userService.saveUser("222r", "Gridi", (byte) 25);
-//        userService.saveUser("Ardfdr", "Babai", (byte) 23);
-//         userService.dropUsersTable();
-//         userService.cleanUsersTable();
-////        userService.removeUserById(3);
-//        userService.getAllUsers();
-//        userService.dropUsersTable();
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Artour", "Babai", (byte) 23);
+        userService.saveUser("222r", "Gridi", (byte) 25);
+        userService.saveUser("Ardfdr", "Babai", (byte) 23);
+        userService.getAllUsers();
         userService.cleanUsersTable();
-//        userService.getAllUsers();
         userService.dropUsersTable();
     }
 }
